62a867a686a8bef89097462f1d9d96679aec12c9
theClubhou.se <electrifiedmedia@gmail.com>
2020-02-07 17:39:54 -0500

    Initial DOM manipulation

--- groceryApp/groceryApp.js
+++ groceryApp/groceryApp.js
@@ -1,43 +1,43 @@
-/*
-// CODE FOR OPTION 1
-// option 1: multideminsional array
-let inventory = [
-    ['apple', 10],
-    ['blueberry', 15],
-    ['broccoli', 5],
-    ['milk', 25],
-    ['cheese', 0]
-];
-function add() {
-    let input = document.getElementById('userInput').value;
-    inventory.forEach(function(array) {
-        if(input == array[0]) {
-            array[1]++;
-            console.log(array);
-        }
-    });
-}
-function sub() {
-    let input = document.getElementById('userInput').value;
-    for(let i = 0; i < inventory.length; i++) {
-        if(input == inventory[i][0]) {
-            if(inventory[i][1] > 1) {
-                inventory[i][1]--;
-                console.log(inventory[i]);
-            } else if(inventory[i][1] == 0) {
-                console.log(`${input} is out of stock!!!`);
-                console.log(inventory[i]);
-            } else {
-                console.log(`${input} is out of stock!!!`);
-                inventory[i][1]--;
-                console.log(inventory[i]);
-            }
-        }
-    } 
-}
-*/
 
-//
+// // CODE FOR OPTION 1
+// // option 1: multideminsional array
+// let inventory = [
+//     ['apple', 10],
+//     ['blueberry', 15],
+//     ['broccoli', 5],
+//     ['milk', 25],
+//     ['cheese', 0]
+// ];
+// function add() {
+//     let input = document.getElementById('userInput').value;
+//     inventory.forEach(function(array) {
+//         if(input == array[0]) {
+//             array[1]++;
+//             console.log(array);
+//         }
+//     });
+// }
+// function sub() {
+//     let input = document.getElementById('userInput').value;
+//     for(let i = 0; i < inventory.length; i++) {
+//         if(input == inventory[i][0]) {
+//             if(inventory[i][1] > 1) {
+//                 inventory[i][1]--;
+//                 console.log(inventory[i]);
+//             } else if(inventory[i][1] == 0) {
+//                 console.log(`${input} is out of stock!!!`);
+//                 console.log(inventory[i]);
+//             } else {
+//                 console.log(`${input} is out of stock!!!`);
+//                 inventory[i][1]--;
+//                 console.log(inventory[i]);
+//             }
+//         }
+//     } 
+// }
+
+
+
 // CODE FOR OPTION 2
 // option 2: parallel array
 let name = ['apple', 'blueberry', ' broccoli', 'milk', 'cheese'];

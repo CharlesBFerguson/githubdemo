0c177647327a5ce3a06d6b70c3b5b240806bc1ac
theClubhou.se <electrifiedmedia@gmail.com>
2020-02-13 08:46:59 -0500

    Country USA else if France

--- DOMmanipulation.js
+++ DOMmanipulation.js
@@ -1,29 +1,56 @@
 //  Exercise Dom manipulation
-
-//  - change styles of 3 separate elements using Javascript:
+//  - change styles of 3 separate elements using Javascript
 //  - change the class name and text of a a child or sibling
 //  of each of the above elements:
 
-document.getElementById("title").textContent = "AMERICA!";
+let country="USA"
+console.log(country)
 
-document.getElementById("unorderedList").style.color = "white";
+  if (country=="USA") {
+    document.getElementById("title").textContent = "AMERICA!";
+    document.getElementById("unorderedList").style.color = "white";
 
-function redBar() {
-  var c = document.getElementById("unorderedList").children;
-  c[0].style.backgroundColor = "red";
-  c[0].textContent = "RED";
-}
-redBar()
+    function redBar() {
+      var c = document.getElementById("unorderedList").children;
+      c[0].style.backgroundColor = "red";
+      c[0].textContent = "RED";
+    }
+    redBar()
 
-function whiteBar() {
-  var c = document.getElementById("unorderedList").children;
-  c[1].textContent = "WHITE";
-}
-whiteBar()
+    function whiteBar() {
+      var c = document.getElementById("unorderedList").children;
+      c[1].textContent = "WHITE";
+    }
+    whiteBar()
 
-function blueBar() {
-  var c = document.getElementById("unorderedList").children;
-  c[2].style.backgroundColor = "blue";
-  c[2].textContent = "BLUE";
-}
-blueBar()
+    function blueBar() {
+      var c = document.getElementById("unorderedList").children;
+      c[2].style.backgroundColor = "blue";
+      c[2].textContent = "BLUE";
+    }
+    blueBar()
+  
+  } else if (country=="France") {
+    document.getElementById("title").textContent = "Vive la France!";
+    document.getElementById("unorderedList").style.color = "white";
+
+    function blueBar() {
+      var c = document.getElementById("unorderedList").children;
+      c[0].style.backgroundColor = "blue";
+      c[0].textContent = "BLUE";
+    }
+    blueBar()
+
+    function whiteBar() {
+      var c = document.getElementById("unorderedList").children;
+      c[1].textContent = "WHITE";
+    }
+    whiteBar()
+
+    function redBar() {
+      var c = document.getElementById("unorderedList").children;
+      c[2].style.backgroundColor = "red";
+      c[2].textContent = "RED";
+    }
+    redBar()
+  }

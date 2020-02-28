0031d3c8f7bc50d4c5edc48825b42012ddd546eb
CharlesBFerguson <fergurburger326@gmail.com>
2020-02-28 14:42:13 -0500

    Homework Week 5

--- Week_5/cmd1.js
+++ Week_5/cmd1.js
@@ -2,4 +2,3 @@
 //  - Create a Javascript file and load it to your command line tool (cmd || powershell || terminal || git bash)
 //  - Create a second Javascript file and call it from the first file using exports
 
-// commit

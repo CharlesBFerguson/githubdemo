53b660bb7a2647366766434896f72cf4823ebe3b
CharlesBFerguson <fergurburger326@gmail.com>
2020-03-04 15:51:17 -0500

    Homework Week 5

--- Week_5/cmd.js
+++ Week_5/cmd.js
@@ -1,6 +1,8 @@
-//  Exercise cmd
-//  - Create a Javascript file and load it to your command line tool (cmd || powershell || terminal || git bash)
-//  - Create a second Javascript file and call it from the first file using exports
+/*
+Exercise cmd
+(/)	- Create a Javascript file and load it to your command line tool (cmd || powershell || terminal || git bash)
+(/)	- Create a second Javascript file and call it from the first file using exports
+*/
 
 const cmdLink = require('./cmd_link');
 cmdLink.on('ready', () => {

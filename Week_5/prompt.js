4eb1dca84b5d443430fa8a56f7071a0b03a8ec03
CharlesBFerguson <fergurburger326@gmail.com>
2020-03-04 15:44:16 -0500

    Homework Week 5

--- Week_5/prompt.js
+++ Week_5/prompt.js
@@ -1,10 +1,11 @@
-//  - Create a Node Prompt that asks a question.
-//  - Then have the prompt print to the console a string with
-//    some of the user's response.
-//  - Then after that question is asked. Ask another question.
-//  - Put the input logic and the output logic into different files
-//    and require both of them into a main file.
-//    Make the main file callable from the cli.
+/*
+Exercise prompt
+(/) - Create a Node Prompt that asks a question.
+(/) - Then have the prompt print to the console a string with some of the user's response.
+    - Then after that question is asked. Ask another question.
+    - Put the input logic and the output logic into different files and require both of them into a main file.
+    - Make the main file callable from the cli.
+*/
 
 // const readline = require('readline').createInterface({
 //   input: process.stdin,

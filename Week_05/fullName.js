63aefa22c6cb78c4114c6ef065a22df7cd5dcfe3
CharlesBFerguson <fergurburger326@gmail.com>
2020-03-09 14:23:08 -0400

    fs.writeFileSync then fs.readFileSync to main file then c.l successful

--- Week_05/fullName.js
+++ Week_05/fullName.js
@@ -3,5 +3,3 @@ const fs = require('fs');
 const fsFirstName = fs.readFileSync('firstName.txt', 'utf8');
 const fsLastName = fs.readFileSync('lastName.txt', 'utf8');
 console.log(`Welcome, ${fsFirstName} ${fsLastName}`)
-
-// YAY!
